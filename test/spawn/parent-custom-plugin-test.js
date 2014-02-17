28b5e3d077468e6c453402e6ca96d64bc595f6ca
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-02-17 15:58:46 +0100

    remove custom plugin tests

--- test/spawn/parent-custom-plugin-test.js
+++ test/spawn/parent-custom-plugin-test.js
@@ -5,7 +5,7 @@
  * MIT LICENCE
  *
  */
- 
+
 var assert = require('assert'),
     path = require('path'),
     exec = require('child_process').exec,
@@ -18,7 +18,7 @@ var assert = require('assert'),
 var jail = path.join(__dirname, '..', '..', 'examples', 'app'),
     custom = path.join(__dirname, '..', 'fixtures', 'custom.js'),
     options;
-    
+
 options = {
   port: 5060,
   script: path.join(jail, 'server.js'),
@@ -26,24 +26,24 @@ options = {
   cwd: process.cwd(),
   keepalive: true
 };
-    
-vows.describe('carapace/spawn/custom-plugin').addBatch({
-  "When using haibu-carapace": {
-    "spawning a child carapace with a custom plugin": helper.assertParentSpawn(options, {
-      "after the plugin is loaded": {
-        topic: function (info, child) {
-          var that = this;
-          child.once('message', function (info) {
-            if (info.event === 'custom') {
-              that.callback(null, child, info);
-            }
-          });
-        },
-        "should emit the `carapace::custom` event": function (_, child, info) {
-          assert.isTrue(info.data.custom);
-          child.kill();
-        }
-      }
-    })
-  }
-}).export(module);
+
+//vows.describe('carapace/spawn/custom-plugin').addBatch({
+  //"When using haibu-carapace": {
+    //"spawning a child carapace with a custom plugin": helper.assertParentSpawn(options, {
+      //"after the plugin is loaded": {
+        //topic: function (info, child) {
+          //var that = this;
+          //child.once('message', function (info) {
+            //if (info.event === 'custom') {
+              //that.callback(null, child, info);
+            //}
+          //});
+        //},
+        //"should emit the `carapace::custom` event": function (_, child, info) {
+          //assert.isTrue(info.data.custom);
+          //child.kill();
+        //}
+      //}
+    //})
+  //}
+//}).export(module);
